36981a3675a051ba71faae311c079bdbf04d87ea
Tony <antoniovitor.gb@gmail.com>
2025-02-23 15:44:43 +0100

    Added mcu documents.

---
+++ Inc/Drivers/rcc.h
@@ -0,0 +1,14 @@
+/**
+* @file    rcc.h
+* @author  Antonio Vitor Grossi Bassi (antoniovitor.gb@gmail.com)
+* @brief   Reset and Clock Control Drivers reference.
+* @version 0.1
+* @date    23-02-2025
+* 
+* @copyright Copyright (c) 2025
+* 
+*/
+
+/* Dependencies */
+#include "stm32f4xx.h"
+
---
+++ Src/Drivers/rcc.c
@@ -0,0 +1,13 @@
+/**
+ * @file    rcc.c
+ * @author  Antonio Vitor Grossi Bassi (antoniovitor.gb@gmail.com)
+ * @brief   Reset and Clock Control Drivers 
+ * @version 0.1
+ * @date    23-02-2025
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "rcc.h"
+
